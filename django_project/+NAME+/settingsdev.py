9d84cdc64195af123b3e518d286cf5d4764d063f
Vince Veselosky <vince@control-escape.com>
2010-10-05 15:06:21 -0400

    Load the debug toolbar middleware

--- django_project/+NAME+/settingsdev.py
+++ django_project/+NAME+/settingsdev.py
@@ -4,5 +4,6 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = '{{ project_name }}.urlsdev'
 INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 CACHE_BACKEND = 'locmem://'
 TEMPLATE_STRING_IF_INVALID = 'INVALID TEMPLATE VARIABLE'

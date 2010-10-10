47e1690300817c3e97b34295d1521e4e98589802
Vince Veselosky <vince@control-escape.com>
2010-10-10 18:41:41 -0400

    Fixed cache key settings bug.
    Can't concatenate a string and an int, this is Python. Also set the dev
    cache backend to dummy, because wow is cache annoying while developing!

--- django_project/+NAME+/settingsdev.py
+++ django_project/+NAME+/settingsdev.py
@@ -5,5 +5,5 @@ TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = '{{ project_name }}.urlsdev'
 INSTALLED_APPS += ('debug_toolbar',)
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-CACHE_BACKEND = 'locmem://'
+CACHE_BACKEND = 'dummy://'
 TEMPLATE_STRING_IF_INVALID = 'INVALID TEMPLATE VARIABLE'

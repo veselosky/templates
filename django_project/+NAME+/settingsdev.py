696d30b5e3d6113edb48bb4d7650689528a6ae39
Vince Veselosky <vince@control-escape.com>
2010-10-10 21:45:17 -0400

    Add config for debug toolbar.

--- django_project/+NAME+/settingsdev.py
+++ django_project/+NAME+/settingsdev.py
@@ -3,7 +3,11 @@ from {{ project_name }}.settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = '{{ project_name }}.urlsdev'
-INSTALLED_APPS += ('debug_toolbar',)
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
 CACHE_BACKEND = 'dummy://'
 TEMPLATE_STRING_IF_INVALID = 'INVALID TEMPLATE VARIABLE'
+
+# Activate and configure django-debug-toolbar
+INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}

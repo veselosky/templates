f1a32ede00c393a0a94084dca995233581ddef6d
Vince Veselosky <vince@control-escape.com>
2010-12-22 14:24:55 -0500

    some boilerplate for generic views

--- django_app/+NAME+/urls.py
+++ django_app/+NAME+/urls.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 from django.conf.urls.defaults import *
+# from django.views.generic.simple import direct_to_template, redirect_to
+# from django.views.generic.list_detail import object_list, object_detail
 
 urlpatterns = patterns('',
     # Example:

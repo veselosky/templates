ebe08efbcbcb18c4675d11e0e0595f16bd6384e4
Vince Veselosky <vince@control-escape.com>
2013-05-02 14:54:29 -0400

    update to pytest conventions and infrastructure

--- pyproject/setup.py
+++ pyproject/setup.py
@@ -1,32 +1,40 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    """Command class to allow `setup.py test` to run py.test.
+
+    http://pytest.org/latest/goodpractises.html#integration-with-setuptools-distribute-test-commands
+    """
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
 setup(
-    name = "{{ project_name }}",
-    version = "0.1",
-    packages = find_packages(),
-    author = "Vince Veselosky",
-    author_email = "vince@control-escape.com",
-    description = "",
-    license = "MIT",
-    url = "",
+    name="{{ project_name }}",
+    version="0.1",
+    packages=find_packages(),
+    author="Vince Veselosky",
+    author_email="vince@control-escape.com",
+    description="",
+    license="MIT",
+    url="",
     # could also include long_description, download_url, classifiers, etc.
 
-    # Project includes Sphinx documentation, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    install_requires = [
-        'argparse >= 1.2.1',
-        'docutils >= 0.7',
-        'python-dateutil >= 1.5, <2.0',
-        'sphinx >= 1.0',
-        'unittest2 >= 0.5.1',
+    install_requires=[
     ],
-
-    include_package_data=True,
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the package, too:
-        '': ['*.msg'],
-    },
-
-
+    tests_require=[
+        'pytest',
+    ],
+    cmdclass={'test': PyTest},
 )
---
+++ pyproject/tests/test_000.py
@@ -0,0 +1,4 @@
+"""
+Tests for {{ project_name }} use the pytest framework.
+"""
+# import pytest
